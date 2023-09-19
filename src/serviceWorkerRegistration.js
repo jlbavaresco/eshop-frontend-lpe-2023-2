64a9a2a4380ee6b4eba1d10b18565a40f91cc02f
Jorge Bavaresco <jlbavaresco@gmail.com>
2023-09-19 10:05:17 -0300

    ajustes

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -68,6 +68,7 @@ function registerValidSW(swUrl, config) {
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
               alert('Nova versão disponível! Feche todas as abas e entre novamente no aplicativo')
+              
               console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.'
